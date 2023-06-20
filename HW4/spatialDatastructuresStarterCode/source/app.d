f44429aa85f3d50bbbe26db8a2c2d2012fc9ef8a
Lydia Yuan <lydiayuan99@gmail.com>
2023-06-20 15:07:19 -0600

    add: timing test

--- HW4/spatialDatastructuresStarterCode/source/app.d
+++ HW4/spatialDatastructuresStarterCode/source/app.d
@@ -3,54 +3,142 @@ import std.stdio;
 import common;
 import dumbknn;
 import bucketknn;
-//import your files here
+import quadtree;
+import kdtree;
 
 void main()
 {
-
-    //because dim is a "compile time parameter" we have to use "static foreach"
-    //to loop through all the dimensions we want to test.
-    //the {{ are necessary because this block basically gets copy/pasted with
-    //dim filled in with 1, 2, 3, ... 7.  The second set of { lets us reuse
-    //variable names.
-    writeln("dumbKNN results");
-    static foreach(dim; 1..8){{
-        //get points of the appropriate dimension
-        auto trainingPoints = getGaussianPoints!dim(1000);
-        auto testingPoints = getUniformPoints!dim(100);
-        auto kd = DumbKNN!dim(trainingPoints);
-        writeln("tree of dimension ", dim, " built");
-        auto sw = StopWatch(AutoStart.no);
-        sw.start; //start my stopwatch
-        foreach(const ref qp; testingPoints){
-            kd.knnQuery(qp, 10);
-        }
-        sw.stop;
-        writeln(dim, sw.peek.total!"usecs"); //output the time elapsed in microseconds
-        //NOTE, I SOMETIMES GOT TOTALLY BOGUS TIMES WHEN TESTING WITH DMD
-        //WHEN YOU TEST WITH LDC, YOU SHOULD GET ACCURATE TIMING INFO...
-    }}
-
-
-    writeln("BucketKNN results");
-    //Same tests for the BucketKNN
-    static foreach(dim; 1..8){{
-        //get points of the appropriate dimension
+    // Time QuadTree with different points per AABB
+    writeln("QuadTree knnQuery with different points per AABB results");
+    // Create a file to write the results
+    string pointsPerAABBFilename = "QuadTreeWithDifferentPointsPerAABB.csv";
+    File pointsPerAABBFile = File(pointsPerAABBFilename, "w");
+    pointsPerAABBFile.writeln("numPointsPerAABB,averageTime");
+    // Calculate the powers of 2 sequence from 1 to 256
+    ulong[] quadTreeNumPointsSequence;
+    for (int i = 0; i <= 8; ++i)
+    {
+        quadTreeNumPointsSequence ~= 2^^i;
+    }
+    foreach (numPoints; quadTreeNumPointsSequence)
+    {{
         enum numTrainingPoints = 1000;
-        auto trainingPoints = getGaussianPoints!dim(numTrainingPoints);
-        auto testingPoints = getUniformPoints!dim(100);
-        auto kd = BucketKNN!dim(trainingPoints, cast(int)pow(numTrainingPoints/64, 1.0/dim)); //rough estimate to get 64 points per cell on average
-        writeln("tree of dimension ", dim, " built");
+        auto trainingPoints = getGaussianPoints!2(numTrainingPoints);
+        auto testingPoints = getUniformPoints!2(100);
+        auto qt = QuadTree!2(trainingPoints);
+        writeln("Tree with ", numPoints, " points per AABB built");
         auto sw = StopWatch(AutoStart.no);
-        sw.start; //start my stopwatch
-        foreach(const ref qp; testingPoints){
-            kd.knnQuery(qp, 10);
+        sw.start;
+        ulong totalTime = 0;
+        size_t experimentTimes = 50;
+        foreach (const ref qp; testingPoints)
+        {
+            for (int i = 0; i < experimentTimes; ++i)
+            {
+                sw.start;
+                qt.knnQuery(qp, 10);
+                sw.stop;
+                totalTime += sw.peek.total!"usecs";
+                sw.reset;
+            }
         }
-        sw.stop;
-        writeln(dim, sw.peek.total!"usecs"); //output the time elapsed in microseconds
-        //NOTE, I SOMETIMES GOT TOTALLY BOGUS TIMES WHEN TESTING WITH DMD
-        //WHEN YOU TEST WITH LDC, YOU SHOULD GET ACCURATE TIMING INFO...
+
+        double averageTime = totalTime / experimentTimes / testingPoints.length;
+        // Write the results to the file
+        pointsPerAABBFile.writeln(numPoints, ",", averageTime);
+        writeln("Average time for ", numPoints, " points: ", averageTime, " usecs");
     }}
 
+    pointsPerAABBFile.close();
+
+    // Time QuadTree with different K values
+    writeln("QuadTree knnQuery with different K results");
+
+    enum numPointsPerAABB = 4;
+
+    // Create a file to write the results
+    string knnFilename = "QuadTreeWithDifferentK.csv";
+    File knnFile = File(knnFilename, "w");
+    knnFile.writeln("K,averageTime");
+
+    // Define different values of K
+    int[] knnValues;
+    for (int k = 10; k <= 100; k += 10)
+    {
+        knnValues ~= k;
+    }
+
+    foreach (knn; knnValues)
+    {{
+        auto trainingPoints = getGaussianPoints!2(1000);
+        auto testingPoints = getUniformPoints!2(100);
+        auto qt = QuadTree!numPointsPerAABB(trainingPoints);
+        auto sw = StopWatch(AutoStart.no);
+        sw.start;
+        ulong totalTime = 0;
+        size_t experimentTimes = 10;
+
+        foreach (const ref qp; testingPoints)
+        {
+            for (int i = 0; i < experimentTimes; ++i)
+            {
+                sw.start;
+                qt.knnQuery(qp, knn);
+                sw.stop;
+                totalTime += sw.peek.total!"usecs";
+                sw.reset;
+            }
+        }
+
+        double averageTime = totalTime / experimentTimes / testingPoints.length;
+        writeln("Average time with K =", knn, ": ", averageTime, " usecs");
+
+        // Write the results to the file
+        knnFile.writeln(knn, ",", averageTime);
+    }}
+
+    knnFile.close();
+
+    writeln("QuadTree knnQuery with different point pool size");
+    string differentPointPoolResultFileName = "QuadTreeWithDifferentPointPoolSize.csv";
+    File differentPointPoolResults = File(differentPointPoolResultFileName, "w");
+    differentPointPoolResults.writeln("pointPoolSize,averageTime");
+
+    // Define different values of K
+    int[] poolSizes;
+    for (int k = 1; k <= 10; k++)
+    {
+        poolSizes ~= k*1000;
+    }
+
+    foreach (trainingPoolSize ; poolSizes)
+    {{
+        auto trainingPoints = getGaussianPoints!2(trainingPoolSize);
+        auto testingPoints = getUniformPoints!2(100);
+        auto qt = QuadTree!numPointsPerAABB(trainingPoints);
+
+        auto sw = StopWatch(AutoStart.no);
+        sw.start;
+        ulong totalTime = 0;
+        size_t experimentTimes = 10;
+
+        foreach (const ref qp; testingPoints)
+        {
+            for (int i = 0; i < experimentTimes; ++i)
+            {
+                sw.start;
+                qt.knnQuery(qp, 10);
+                sw.stop;
+                totalTime += sw.peek.total!"usecs";
+                sw.reset;
+            }
+        }
+
+        double averageTime = totalTime / experimentTimes / testingPoints.length;
+        writeln("Average time with point pool size =", trainingPoolSize, ": ", averageTime, " usecs");
+        differentPointPoolResults.writeln(trainingPoolSize, ",", averageTime);
+    }}
+
+    differentPointPoolResults.close();
 
 }
